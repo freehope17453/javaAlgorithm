eed0da59a0c235a35a942339f300e98847eb5e88
freehope17453 <freehope_9@live.com>
2018-06-06 13:50:05 +0800

    插入排序

---
+++ InsertionSort.java
@@ -0,0 +1,31 @@
+public class InsertionSort{
+	public static void main(String[] args){
+		int[] a = {5,2,4,6,1,3};
+		for(int i=0;i<a.length;i++){
+			System.out.print(a[i]);
+			System.out.print(",");
+		}
+		System.out.println();
+		a = insertionSort_(a);
+		for(int i=0;i<a.length;i++){
+			System.out.print(a[i]);
+			System.out.print(",");
+		}
+	}
+
+
+
+	public static int[] insertionSort_(int[] sortData){
+		for(int j=1;j<sortData.length;j++){
+			int key = sortData[j];
+			int i = j-1;
+			while(i>=0&&sortData[i]>key){
+				sortData[i+1] = sortData[i];
+				i--;
+			}
+			sortData[i+1]=key;
+		}
+		return sortData;
+	}
+}
+
